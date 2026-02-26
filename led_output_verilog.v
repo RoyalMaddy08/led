6197256950aedcf436b11eabd13baea91a99c8ff
RoyalMaddy08 <133629919+RoyalMaddy08@users.noreply.github.com>
2026-02-27 00:42:10 +0530

    Add files via upload

---
+++ led_output_verilog.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 24.02.2026 21:37:20
+// Design Name: 
+// Module Name: led
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module led(
+   input sw,
+   output led
+    );
+  assign led=sw;
+endmodule
